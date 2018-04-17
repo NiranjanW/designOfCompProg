9bb1f3a856032d186f0df4ff7d2462ded217d633
niranjanw <niranjanw@gmail.com>
2018-04-16 20:35:03 -0400

    'added new test'

--- probSet1/poker.py
+++ probSet1/poker.py
@@ -7,8 +7,17 @@ def hand_rank(hand):
 
 	ranks =card_rank(hand)
 
-	if straight(ranks):
-		return
+	if straight(ranks) and flush(hand):
+		return (8,max(ranks))
+	elif kind(4,ranks):
+		return (7, )
+
+def card_rank(hand):
+	"Return a list of hands sorted  with higher order"
+	ranks = ['--23456789TJQKA'.index(r) for r , s in hand]
+	ranks.sort(reversed =True)
+	return ranks
+
 
 def test():
 	"test cases for funtions in poker"
@@ -17,5 +26,9 @@ def test():
 	fk ="9D 9H 9S 9C 7D".split()
 	fh-"TD TC TH 7C 7D".split()
 	assert poker([sf ,fk ,fh]) == sf
+	assert poker([fk, fh]) == fk
+    assert poker([fh, fh]) == fh
+	assert poker([sf]) == sf
+    assert poker([sf] + 99*[fh]) == sf
 
 
